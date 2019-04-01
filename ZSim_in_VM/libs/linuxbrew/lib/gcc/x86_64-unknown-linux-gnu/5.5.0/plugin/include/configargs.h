307b76c2534a67fcd7375152138c8319b53f1d1e
vagrant <vagrant@precise64.(none)>
2019-04-01 20:19:10 +0000

    5th

--- ZSim_in_VM/libs/linuxbrew/lib/gcc/x86_64-unknown-linux-gnu/5.5.0/plugin/include/configargs.h
+++
@@ -1,7 +0,0 @@
-/* Generated automatically. */
-static const char configuration_arguments[] = "../configure --with-isl=/home/aming/.linuxbrew/opt/isl@0.18 --with-bugurl=https://github.com/Linuxbrew/homebrew-core/issues --prefix=/home/aming/.linuxbrew/Cellar/gcc/5.5.0_4 --enable-languages=c,c++,objc,obj-c++,fortran --program-suffix=-5 --with-gmp=/home/aming/.linuxbrew/opt/gmp --with-mpfr=/home/aming/.linuxbrew/opt/mpfr --with-mpc=/home/aming/.linuxbrew/opt/libmpc --enable-stage1-checking --enable-checking=release --enable-lto --with-build-config=bootstrap-debug --disable-werror --with-pkgversion='Homebrew gcc 5.5.0_4' --with-boot-ldflags='-static-libstdc++ -static-libgcc ' --disable-nls --disable-multilib";
-static const char thread_model[] = "posix";
-
-static const struct {
-  const char *name, *value;
-} configure_default_options[] = { { "cpu", "generic" }, { "arch", "x86-64" } };
